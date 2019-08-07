35918af4ad53454e9dc1206a612a229967874a1b
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-08-06 22:09:03 -0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omstack8:omstack8@cluster0-gzwkz.mongodb.net/omstack8?retryWrites=true&w=majority',
+    {useNewUrlParser : true}
+);
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
